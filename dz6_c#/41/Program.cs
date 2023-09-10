8ec0ba58fcc47be27b0cc312442733031b71109d
Alla <mymail@exampl.com>
2023-09-09 20:22:22 -0400

    dz_6

---
+++ dz6_c#/41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void Printing(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i=0; i<arr.Length; i++)
+    System.Console.Write($"{arr[i]} ");
+    System.Console.Write("]");
+};
+
+int Check(int[] arr)
+{
+    int count = 0;
+    for (int i=0; i<arr.Length; i++)
+    if (arr[i]>0)
+    count ++;
+    return count;
+}
+
+System.Console.WriteLine("Vvedite 4 chisla");
+int[] arr = new int[4];
+
+for (int i = 0; i < 4; i++){
+int num=Convert.ToInt32(System.Console.ReadLine( ));
+arr[i]=num;
+}
+
+Printing(arr);
+Check(arr);
+System.Console.WriteLine();
+System.Console.WriteLine(Check(arr));
---
+++ dz6_c#/43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] LineData1 = new double[2];
+LineData1[0] = 2;
+LineData1[1] = 4;
+double[] LineData2 = new double[2];
+LineData2[0] = 0;
+LineData2[1] = 3;
+
+double[] FindCoord(double [] LineData1, double [] LineData2){
+    double [] coord = new double[2];
+    coord[0] = (LineData1[1]-LineData2[1])/(LineData2[0]-LineData1[0]);
+    coord[1] = LineData1[1]*coord[0]+LineData1[1];
+    System.Console.WriteLine(coord[0]);
+    System.Console.WriteLine(coord[1]);
+    return coord;
+}
+
+FindCoord(LineData1, LineData2);
